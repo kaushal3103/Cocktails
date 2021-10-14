be9730ed2253140c4a63fde113b2a92383181207
kaushal3103 <81116597+kaushal3103@users.noreply.github.com>
2021-10-14 16:53:59 +0530

    Add files via upload

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => {
+  return (
+   <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Expedita sint quisquam deserunt ea fugiat obcaecati,
+        quibusdam dolores quidem repellendus.Dolores ratione 
+        odit earum fuga nesciunt quasi quidem molestias sapiente voluptates.
+      </p>
+    </section>
+  )
+}
+
+export default About
